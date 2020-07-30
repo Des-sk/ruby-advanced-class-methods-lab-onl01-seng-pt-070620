8a1eefab4804d2f310fbde6b3dcbabcb51225e96
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-30 04:23:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
  end 
  
  def self.find_or_create_by_name(name)
-   @@all.find {|song| song.name==name} || @@all.create{|song| song.name==name}
+   @@all.find {|song| song.name==name} || @@all.create{|song| song=self.new}
 end 
 
 end
