791c4cea6372fac047f17796091ec1e42fcff484
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-30 04:46:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,5 @@ class Song
 end 
 
 def self.alphabetical
-  @@all.collect do |name| 
-  song.name.sort_by {|name| song.name.alphabetical}
-  #self.sort_by {|song| song.name.alphabetical}
-end
+  @@all.sort_by
 end
